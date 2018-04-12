2f90af04df2cb054ecd8f8671608d58902479ab1
Anant Maheshwari <anant@Anants-MacBook-Pro.local>
2018-04-12 19:10:45 -0400

    add mmr_t

--- deliverables/baselines.py
+++ deliverables/baselines.py
@@ -175,6 +175,53 @@ def extract_absolute_difference(s1, s2):
         t5 = abs(cnt1 - cnt2) / float(cnt1 + cnt2)
     return [t1, t2, t3, t4, t5]
 
+def extract_mmr_t(s1, s2):
+    shorter = 1
+    if(len(s1) > len(s2)):
+        shorter = 2
+
+    s1, s2 = word_tokenize(s1), word_tokenize(s2)
+    pos1, pos2 = pos_tag(s1), pos_tag(s2)
+    # all tokens
+    t1 = len(s1) / len(s2)
+    # all adjectives
+    cnt1 = len([1 for item in pos1 if item[1].startswith('J')])
+    cnt2 = len([1 for item in pos2 if item[1].startswith('J')])
+    if cnt1 == 0 and cnt2 == 0:
+        t2 = 0
+    else:
+        t2 = cnt1 / cnt2
+    # all adverbs
+    cnt1 = len([1 for item in pos1 if item[1].startswith('R')])
+    cnt2 = len([1 for item in pos2 if item[1].startswith('R')])
+    if cnt1 == 0 and cnt2 == 0:
+        t3 = 0
+    else:
+        t3 = cnt1 / cnt2
+    # all nouns
+    cnt1 = len([1 for item in pos1 if item[1].startswith('N')])
+    cnt2 = len([1 for item in pos2 if item[1].startswith('N')])
+    if cnt1 == 0 and cnt2 == 0:
+        t4 = 0
+    else:
+        t4 = cnt1 / cnt2
+    # all verbs
+    cnt1 = len([1 for item in pos1 if item[1].startswith('V')])
+    cnt2 = len([1 for item in pos2 if item[1].startswith('V')])
+    if cnt1 == 0 and cnt2 == 0:
+        t5 = 0
+    else:
+        t5 = cnt1 / cnt2
+
+    if shorter == 2:
+        t1 = 1 / t1
+        t2 = 1 / t2
+        t3 = 1 / t3
+        t4 = 1 / t4
+        t5 = 1 / t5
+
+    return [t1, t2, t3, t4, t5]
+
 #--------------------
 # from sklearn.svm import SVC, LinearSVC
 from sklearn.linear_model import LinearRegression
@@ -205,7 +252,8 @@ def main(args):
         scores = [ #sentence_similarity_simple_baseline(s1,s2),
                    sentence_similarity_word_alignment(s1,s2),
                    extract_overlap_pen(s1, s2),
-                    *extract_absolute_difference(s1, s2)
+                    *extract_absolute_difference(s1, s2),
+                    *extract_mmr_t(s1, s2)
                    ]
         # cosine similarity
         feature_scores.append(scores)
