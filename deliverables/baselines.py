09fef6f231048b3e2df332c0b7dfb487b0bc7ee8
Yezheng Li <tiankongsky1102@gmail.com>
2018-04-13 02:04:31 -0400

    0.1 -> 0.001

--- deliverables/baselines.py
+++ deliverables/baselines.py
@@ -182,42 +182,42 @@ def extract_mmr_t(s1, s2):
     s1, s2 = word_tokenize(s1), word_tokenize(s2)
     pos1, pos2 = pos_tag(s1), pos_tag(s2)
     # all tokens
-    t1 = (len(s1)+0.1) / (len(s2) +0.1)
+    t1 = (len(s1)+0.001) / (len(s2) +0.001)
     # all adjectives
     cnt1 = len([1 for item in pos1 if item[1].startswith('J')])
     cnt2 = len([1 for item in pos2 if item[1].startswith('J')])
     if cnt1 == 0 and cnt2 == 0:
         t2 = 0
     else:
-        t2 = (cnt1 +0.1) / (cnt2 + 0.1)
+        t2 = (cnt1 +0.001) / (cnt2 + 0.001)
     # all adverbs
     cnt1 = len([1 for item in pos1 if item[1].startswith('R')])
     cnt2 = len([1 for item in pos2 if item[1].startswith('R')])
     if cnt1 == 0 and cnt2 == 0:
         t3 = 0
     else:
-        t3 = (cnt1 +0.1) / (cnt2+0.1)
+        t3 = (cnt1 +0.001) / (cnt2+0.001)
     # all nouns
     cnt1 = len([1 for item in pos1 if item[1].startswith('N')])
     cnt2 = len([1 for item in pos2 if item[1].startswith('N')])
     if cnt1 == 0 and cnt2 == 0:
         t4 = 0
     else:
-        t4 = (cnt1 +0.1) / (cnt2 +0.1)
+        t4 = (cnt1 +0.001) / (cnt2 +0.001)
     # all verbs
     cnt1 = len([1 for item in pos1 if item[1].startswith('V')])
     cnt2 = len([1 for item in pos2 if item[1].startswith('V')])
     if cnt1 == 0 and cnt2 == 0:
         t5 = 0
     else:
-        t5 = (cnt1+ 0.1) / (cnt2 + 0.1)
+        t5 = (cnt1+ 0.001) / (cnt2 + 0.001)
 
     if shorter == 2:
-        t1 = 1 / (t1 + 0.1)
-        t2 = 1 / (t2 + 0.1)
-        t3 = 1 / (t3 + 0.1)
-        t4 = 1 / (t4 + 0.1)
-        t5 = 1 / (t5 + 0.1)
+        t1 = 1 / (t1 + 0.001)
+        t2 = 1 / (t2 + 0.001)
+        t3 = 1 / (t3 + 0.001)
+        t4 = 1 / (t4 + 0.001)
+        t5 = 1 / (t5 + 0.001)
 
     return [t1, t2, t3, t4, t5]
 
